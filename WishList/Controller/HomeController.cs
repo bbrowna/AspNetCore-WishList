44a8f91cab5f5e2349bdd6ed77e95b0cb7ce40eb
Brittney Brown <bbrowna486@gmail.com>
2020-11-30 12:44:22 -0600

    Added index and error views and home controller

---
+++ WishList/Controller/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Controllers;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WishList.Controller
+{
+    public class HomeController
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
