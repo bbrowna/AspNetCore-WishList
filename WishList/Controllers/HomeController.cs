c6885166c9dac84f0526bd887a80416bbbc40825
Brittney Brown <bbrowna486@gmail.com>
2020-11-30 12:56:29 -0600

    Added index and error views and home controller- updated

--- WishList/Controllers/HomeController.cs
+++ WishList/Controllers/HomeController.cs
@@ -15,5 +15,10 @@ namespace WishList.Controllers
         {
             return View();
         }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
     }
 }
