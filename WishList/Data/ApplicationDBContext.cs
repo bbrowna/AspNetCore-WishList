2dccc8a0dc79ceffc6f3da80f821b4a847acfc85
Brittney Brown <bbrowna486@gmail.com>
2020-11-30 19:52:21 -0600

    added ApplicationDBContext and Item model

--- WishList/Data/ApplicationDBContext.cs
+++ WishList/Data/ApplicationDBContext.cs
@@ -6,6 +6,11 @@ namespace WishList.Data
 {
     public class ApplicationDBContext : DbContext 
     {
+
+        public ApplicationDBContext(DbContextOptions options) : base(options)
+        {
+
+        }
         public DbSet<Item> Items { get; set; }
     }
 }
