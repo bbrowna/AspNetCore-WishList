b4e7ee05e895fec099dfd7ddded1ffee82057165
Brittney Brown <bbrowna486@gmail.com>
2020-12-03 13:21:50 -0600

    added delete/create controllers

---
+++ WishList/Controllers/ItemController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using WishList.Data;
+using WishList.Models;
+
+namespace WishList.Controllers 
+{
+    public class ItemController : Controller
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        public ItemController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public ActionResult<Item> Index()
+        {
+            return View("Index", _context.Items);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View("Create");
+        }
+
+        [HttpPost]
+        public IActionResult Create(Item item)
+        {
+
+            _context.Items.Add(item);
+            _context.SaveChanges();
+            return (RedirectToAction());
+        }
+    }
+}
